d52f7f8b66e2baa593317ce4d2fc9a000bca9cda
Nandish Patel <nandishpatel494@gmail.com>
2025-11-29 12:35:09 +0530

    Updated rd estimates code

--- SRC/08_rd_estimates.R
+++ SRC/08_rd_estimates.R
@@ -11,43 +11,41 @@ df_controls <- read.csv("DATA/df_controls.csv")
 
 #Main RDD statewise.
 
-#Creating list variables to store regression results.
-srdd <- list()
-srdd2 <- list()
-
-#Running statewise RDD with and without controls using riots post 2018 as outcome.
-for (i in c("karnataka", "andhra pradesh", "telangana", "uttarpradesh",
-	"rajasthan", "jharkhand", "chhatisgarh", "madhya pradesh", "mizoram",
-	"maharashtra", "bihar", "gujarat", "odisha", "assam")) {
-  df <- df_controls[df_controls$STATE == i, ]
-  z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
-                      df$pc18_work_share, df$pc18_rural_share,
-                      df$pc18_lit_share, df$pc18_forest_share)
-  
-  srdd[[i]] <- rdrobust(y = df$pm25_avg18, x = df$d2c,  all = TRUE, covs = z_controls)
-  srdd2[[i]] <- rdrobust(y = df$pm25_avg18, x = df$d2c,  all = TRUE)
-}
-
-
-#Running statewise RDD with and without controls using riots post 2018 as outcome.
-for (i in c("andhra pradesh", "telangana", "uttarpradesh",
-	"rajasthan", "jharkhand", "chhatisgarh", "madhya pradesh", "mizoram",
-	"maharashtra", "bihar", "gujarat", "odisha", "assam")) {
-  df <- df_controls[df_controls$STATE == i, ]
-  z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
-                      df$pc18_work_share, df$pc18_rural_share,
-                      df$pc18_lit_share, df$pc18_forest_share)
-  
-  srdd[[i]] <- rdrobust(y = df$pm25_max18, x = df$d2c,  all = TRUE, covs = z_controls)
-  srdd2[[i]] <- rdrobust(y = df$pm25_max18, x = df$d2c,  all = TRUE)
-}
-
-####### CHATGPT
+# #Creating list variables to store regression results.
+# srdd <- list()
+# srdd2 <- list()
+#
+# #Running statewise RDD with and without controls using riots post 2018 as outcome.
+# for (i in c("karnataka", "andhra pradesh", "telangana", "uttarpradesh",
+# 	"rajasthan", "jharkhand", "chhatisgarh", "madhya pradesh", "mizoram",
+# 	"maharashtra", "bihar", "gujarat", "odisha", "assam")) {
+#   df <- df_controls[df_controls$STATE == i, ]
+#   z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
+#                       df$pc18_work_share, df$pc18_rural_share,
+#                       df$pc18_lit_share, df$pc18_forest_share)
+#
+#   srdd[[i]] <- rdrobust(y = df$pm25_avg18, x = df$d2c,  all = TRUE, covs = z_controls)
+#   srdd2[[i]] <- rdrobust(y = df$pm25_avg18, x = df$d2c,  all = TRUE)
+# }
+#
+#
+# #Running statewise RDD with and without controls using riots post 2018 as outcome.
+# for (i in c("andhra pradesh", "telangana", "uttarpradesh",
+# 	"rajasthan", "jharkhand", "chhatisgarh", "madhya pradesh", "mizoram",
+# 	"maharashtra", "bihar", "gujarat", "odisha", "assam")) {
+#   df <- df_controls[df_controls$STATE == i, ]
+#   z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
+#                       df$pc18_work_share, df$pc18_rural_share,
+#                       df$pc18_lit_share, df$pc18_forest_share)
+#
+#   srdd[[i]] <- rdrobust(y = df$pm25_max18, x = df$d2c,  all = TRUE, covs = z_controls)
+#   srdd2[[i]] <- rdrobust(y = df$pm25_max18, x = df$d2c,  all = TRUE)
+# }
 
 srdd <- list()
 srdd2 <- list()
 
-# Running statewise RDD with and without controls using pm25_max18 as outcome
+# Running statewise RDD with and without controls
 state_list <- c(
   "karnataka",
   "kerala",
@@ -82,7 +80,7 @@ for (i in state_list) {
   
   # ---- Attempt RDD with controls ----
   srdd[[i]] <- tryCatch({
-    rdrobust(y = df$pm25_avg16, x = df$d2c, all = TRUE, covs = z_controls)
+    rdrobust(y = df$pm25_avg18, x = df$d2c, all = TRUE, covs = z_controls)
   }, error = function(e) {
     message("  RDD with controls failed: ", conditionMessage(e))
     return(NULL)   # skip this state for srdd
@@ -90,14 +88,12 @@ for (i in state_list) {
   
   # ---- Attempt RDD without controls ----
   srdd2[[i]] <- tryCatch({
-    rdrobust(y = df$pm25_avg16, x = df$d2c, all = TRUE)
+    rdrobust(y = df$pm25_avg18, x = df$d2c, all = TRUE)
   }, error = function(e) {
     message("  RDD without controls failed: ", conditionMessage(e))
     return(NULL)   # skip this state for srdd2
   })
 }
-
-
 
 
 #Reporting main results state-wise.
@@ -177,17 +173,14 @@ summary(srdd2[["tripura"]])
 summary(srdd[["uttarpradesh"]])
 summary(srdd2[["uttarpradesh"]])
 
-#Running aggregate RDD with and without controls using riots post 2018 as outcome.
+#Running aggregate RDD with and without controls
 
 #Copying final data into a temporary df.
 df <- df_controls
 
-df <- df %>% filter(!STATE == "uttarpradesh" )
-df <- df %>% filter(!STATE == "mizoram" )
-
 #Creating matrix for state fixed effects.
 states_fe <- model.matrix(~ df$STATE - 1)
-states_fe <- states_fe[, -c(17)]
+states_fe <- states_fe[, -c(19)]
 
 #Binding all controls together.
 z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
@@ -204,28 +197,3 @@ summary(srdd3)
 srdd4 <- rdrobust(y = df$pm25_avg18,
                  x = df$d2c,  all = TRUE, cluster = df$STATE)
 summary(srdd4)
-
-#Placebo test.
-#Using pre 2018 riots as outcome. Only checking for CHHATISGARH and ODISHA.
-
-#CHHATISGARH
-df <- df_controls[df_controls$STATE == "chhatisgarh", ]
-
-z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
-                    df$pc18_work_share, df$pc18_rural_share,
-                    df$pc18_lit_share, df$pc18_forest_share)
-
-srdd5 <- rdrobust(y = df$rt_2016 + df$rt_2017,
-                 x = df$d2c_18,  all = TRUE, covs = z_controls)
-summary(srdd5)
-
-#ODISHA
-df <- df_controls[df_controls$STATE == "odisha", ]
-
-z_controls <- cbind(df$pc18_sc_share, df$pc18_st_share,
-                    df$pc18_work_share, df$pc18_rural_share,
-                    df$pc18_lit_share, df$pc18_forest_share)
-
-srdd6 <- rdrobust(y = df$rt_2016 + df$rt_2017,
-                 x = df$d2c_18,  all = TRUE, covs = z_controls)
-summary(srdd6)
